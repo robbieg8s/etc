331f32c1235b38f04a1ccb5032b33faf35cc8463
Robbie Gates <robbie@halfyak.org>
2023-10-29 11:59:51 +1100

    Add some standard home subdirectories setup.zsh

--- libexec/setup.zsh
+++ libexec/setup.zsh
@@ -6,5 +6,9 @@ set -e
 
 ETC_DIR="${$(print -P '%x'):A:h:h}"
 
+# Standard directories i want
+mkdir -p ~/src
+mkdir -p ~/tmp
+
 # Link configuration with fixed locations
 noglob "${ETC_DIR}"/libexec/link-config.zsh ~/. "${ETC_DIR}"/dotfiles/*
