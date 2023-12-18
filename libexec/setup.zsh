54ed659fe3aee4f8edbd013b243fe81df1c4de2f
Robbie Gates <robbie@halfyak.org>
2023-12-18 15:05:51 +1100

    Use relative links to dotfiles
    I needed to mount my dotfiles into a docker container the other day, and
    making them relative avoid needing to mount them to the same absolute
    location inside the container.

--- libexec/setup.zsh
+++ libexec/setup.zsh
@@ -4,6 +4,9 @@
 
 set -e
 
+# :h:h removes libexec/setup.zsh and is the etc root.
+# While this could be $(git rev-parse --absolute-git-dir), i want
+# to avoid requiring git for setup.zsh to function correctly
 ETC_DIR="${$(print -P '%x'):A:h:h}"
 
 # Standard directories i want
@@ -11,4 +14,8 @@ mkdir -p ~/src
 mkdir -p ~/tmp
 
 # Link configuration with fixed locations
-noglob "${ETC_DIR}"/libexec/link-config.zsh ~/. "${ETC_DIR}"/dotfiles/*
+
+# This re-relativizes the absolute path to the dotfiles directory with respect
+# to ~, so that the links are relative if possible. If not, it will fall back to absolute.
+# Keeping them relative makes mounting them into docker containers easier
+noglob "${ETC_DIR}"/libexec/link-config.zsh ~/. "${ETC_DIR#${HOME:A}/}"/dotfiles/*
